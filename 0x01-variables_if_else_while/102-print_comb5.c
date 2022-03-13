d43867347879b7dc0617ac19b6b6d22a8b551db8
abela12 <abelberhanu6@gmail.com>
2022-03-12 23:07:55 -0600

    102-print_comb5.c - Updated

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,49 +1,31 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - create 2 pairs of number
+ * Return: 0
  */
 int main(void)
 {
-	int p = 48, q = 48, y = 48, x = 49;
+	int i, j;
 
-	while (p < 58)
+	for (i = 0; i <= 99; i++)
 	{
-		while (q < 58)
+		for (j = i; j <= 99; j++)
 		{
-			while (y < 58)
+			if (j != i)
 			{
-				while (x < 58)
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
+				putchar(' ');
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
+
+				if (i * 100 + j != 9899)
 				{
-					putchar(p);
-					putchar(q);
+					putchar(',');
 					putchar(' ');
-					putchar(y);
-					putchar(x);
-					if (!(p == 57 &&
-						q == 56 &&
-						y == 57 &&
-						x == 57))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					x++;
 				}
-				y++;
-				x = 48;
 			}
-			q++;
-			q = p;
-			x = q + 1;
 		}
-		p++;
-		q = 48;
-		y = p;
-		x = q+ 1;
 	}
 	putchar('\n');
 	return (0);
