d5741d4994835e86bc6062dc12cd6e786d716d9d
abela12 <abelberhanu6@gmail.com>
2022-03-15 14:26:29 -0600

    Main header - Added

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,43 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* _putchar.c */
+int _putchar(char);
+
+/* 1-alphabet.c */
+void print_alphabet(void);
+
+/* 2-print_alphabet_x10.c */
+void print_alphabet_x10(void);
+
+/* 3-islower.c */
+int _islower(int);
+
+/* 4-alpha.c */
+int _isalpha(int);
+
+/* 5-sign.c */
+int print_sign(int);
+
+/* 6-abs.c */
+int _abs(int);
+
+/* 7-print_last_digit.c */
+int print_last_digit(int);
+
+/* 8-24_hours.c */
+void jack_bauer(void);
+
+/* 9-times_table.c */
+void times_table(void);
+
+/* 10-add.c */
+int add(int, int);
+
+/* 11-print_to_98.c */
+void print_to_98(int);
+
+/* 100-times_table.c*/
+void print_times_table(int);
+
+#endif
