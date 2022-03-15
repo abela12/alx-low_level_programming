8095b3627e5b19f0a7a51f337dfbfd5afe674ce1
abela12 <abelberhanu6@gmail.com>
2022-03-15 14:21:57 -0600

    4-isalpha.c -Added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Prints 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
