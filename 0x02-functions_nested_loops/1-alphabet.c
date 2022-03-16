79d7063306adc9728a2b685a5111d15dae061993
abela12 <abelberhanu6@gmail.com>
2022-03-15 21:15:12 -0600

    1-alphabet.c - Added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		_putchar (alp);
+		alp++;
+
+	}
+	_putchar ('\n');
+
+}
