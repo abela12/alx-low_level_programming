a2bb429221cea99d236ddf7e18f232bd79210855
abela12 <abelberhanu6@gmail.com>
2022-03-15 21:42:30 -0600

    100-times_table - Updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,70 +1,49 @@
-#include "holberton.h"
-/**
- * putThreeDigits - print 3 digits
- * @prod: product of main loop
- */
-void putThreeDigits(int prod)
-{
-	_putchar(prod / 100 + 48);
-	_putchar((prod / 10) % 10 + 48);
-	_putchar(prod % 10 + 48);
-}
+#include "main.h"
 
 /**
- * putTwoDigits - print 2 digits
- * @prod: product of main loop
- */
-void putTwoDigits(int prod)
-{
-	_putchar(' ');
-	_putchar(prod / 10 + 48);
-	_putchar(prod % 10 + 48);
-}
-/**
- * putOneDigit - print 1 digit
- * @prod: product of main loop
- */
-void putOneDigit(int prod)
-{
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(prod + 48);
-}
-
-/**
- * print_times_table - print multiplication table for factor of n
- * @n: integer n
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
-	int i, j, prod;
-
-	if (n < 16 && n >= 0)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				if (j == 0)
-				{
-					_putchar(48);
-					continue;
-				}
-				prod = i * j;
-				_putchar(',');
-				_putchar(' ');
-				if (prod >= 100)
-				{
-					putThreeDigits(prod);
-				} else if (prod >= 10)
-				{
-					putTwoDigits(prod);
-				} else
-				{
-					putOneDigit(prod);
-				}
-			}
-		_putchar('\n');
-		}
-	}
+int i, j, k;
+if (n >= 0 && n <= 15)
+{
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+k = j * i;
+if (j == 0)
+{
+_putchar(k + '0');
+}
+else if (k < 10 && j != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else if (k >= 10 && k < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+else if (k >= 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 100) + '0');
+_putchar(((k / 10) % 10) + '0');
+_putchar((k % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
 }
