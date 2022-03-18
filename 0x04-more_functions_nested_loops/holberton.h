44dd41ce99c74853c51696608ffa2754f27f7724
abela12 <abelberhanu6@gmail.com>
2022-03-17 21:30:19 -0600

    main file - added

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,15 @@
+#ifndef MAIN
+#define MAIN
+extern int _putchar(char c);
+extern int _isupper(int c);
+extern int _isdigit(int c);
+extern int mul(int a, int b);
+extern void print_numbers(void);
+extern void print_most_numbers(void);
+extern void more_numbers(void);
+extern void print_line(int n);
+extern void print_diagonal(int n);
+extern void print_square(int size);
+extern void print_triangle(int size);
+extern void print_number(int n);
+#endif
